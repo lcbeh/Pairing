530a5cbc89081cd3e169c118034acfff70a899d2
LiChien <lichien2010@yahoo.co.uk>
2016-09-21 10:58:58 +0000

    Green - FizzBuzz methods

--- spec/fizzbuzz2_spec.rb
+++ spec/fizzbuzz2_spec.rb
@@ -1,16 +1,16 @@
-require 'fizzbuzz'
+require 'fizzbuzz2'
 
 describe 'fizzbuzz' do
     it 'returns "fizzbuzz" when passed multiples of 15' do
-        expect(fizzbuzz(15)).to eq 'fizzbuzz'
+        expect(15.fizzbuzz).to eq 'fizzbuzz'
     end
     it 'returns "fizz" when passed multiples of 3' do
-        expect(fizzbuzz(3)).to eq 'fizz'
+        expect(3.fizzbuzz).to eq 'fizz'
     end
     it 'returns "buzz" when passed multiples of 5' do
-        expect(fizzbuzz(5)).to eq 'buzz'
+        expect(5.fizzbuzz).to eq 'buzz'
     end
     it 'returns number when not multiples 3 or 5' do
-        expect(fizzbuzz(4)).to eq 4
+        expect(4.fizzbuzz).to eq 4
     end
 end
